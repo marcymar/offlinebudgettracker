019bd291e3784ec9b5070bcece88d07ae14bcb0e
Marcella Luz Ash <marcella@ash-photos.com>
2020-09-15 17:28:09 -0700

    re wrote icon link

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
